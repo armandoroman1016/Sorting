149d804a1788a6a97c4ea5eb0822d36c6aef2376
Armando Roman <armandoroman1016@gmail.com>
2019-12-09 13:51:49 -0800

    smallest index found on selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,22 +1,27 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
+
+    smallest_num = None
+
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
+        # smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
+        # (hint, can do in 3 loc)
+        if cur_index == 0 or arr[cur_index] < smallest_num:
+            smallest_num = arr[cur_index]
+            smallest_index = cur_index
 
 
         # TO-DO: swap
 
-
+    print(smallest_index)
 
 
     return arr
 
+selection_sort([1,4,3,6,-10,25])
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
